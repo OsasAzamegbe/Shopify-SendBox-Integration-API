fdaffdd447aeaf2b46f08ebda2ad190818084280
osamudiamen <osas.azamegbe@gmail.com>
2020-09-11 18:31:35 +0100

    ran collectstatic. added request and response json documentation for shipping_rates view

--- shopify_sendbox/api/views.py
+++ shopify_sendbox/api/views.py
@@ -38,7 +38,95 @@ def get_shipping_quotes(origin_country, origin_state, origin_city, destination_c
 @api_view(['POST'])
 def shipping_rates(request, *args, **kwargs):
     """
-    Return Shipping Rates data from SendBox to Shopify Carrier Services API
+    Return Shipping Rates data from SendBox to Shopify Carrier Services API.
+
+    Request body should contain shipping info in json format for the required shipping rates to be calculated. 
+
+    A sample request body is shown below:
+
+            {
+                "rate": {
+                    "origin": {
+                    "country": "Nigeria",
+                    "postal_code": "K2P1L4",
+                    "province": "LA",
+                    "city": "Yaba",
+                    "name": null,
+                    "address1": "150 Elgin St.",
+                    "address2": "",
+                    "address3": null,
+                    "phone": "16135551212",
+                    "fax": null,
+                    "email": null,
+                    "address_type": null,
+                    "company_name": "Jamie D's Emporium"
+                    },
+                    "destination": {
+                    "country": "Ghana",
+                    "postal_code": "K1M1M4",
+                    "province": null,
+                    "city": "Accra",
+                    "name": "Bob Norman",
+                    "address1": "24 Sussex Dr.",
+                    "address2": "",
+                    "address3": null,
+                    "phone": null,
+                    "fax": null,
+                    "email": null,
+                    "address_type": null,
+                    "company_name": null
+                    },
+                    "items": [{
+                    "name": "Short Sleeve T-Shirt",
+                    "sku": "",
+                    "quantity": 1,
+                    "grams": 1000,
+                    "price": 1999,
+                    "vendor": "Jamie D's Emporium",
+                    "requires_shipping": true,
+                    "taxable": true,
+                    "fulfillment_service": "manual",
+                    "properties": null,
+                    "product_id": 48447225880,
+                    "variant_id": 258644705304
+                    },
+                    {
+                    "name": "Long Sleeve T-Shirt",
+                    "sku": "",
+                    "quantity": 1,
+                    "grams": 4000,
+                    "price": 1999,
+                    "vendor": "Jamie D's Emporium",
+                    "requires_shipping": true,
+                    "taxable": true,
+                    "fulfillment_service": "manual",
+                    "properties": null,
+                    "product_id": 48447200880,
+                    "variant_id": 258644005304
+                    }],
+                    "currency": "USD",
+                    "locale": "en"
+                }
+            }
+
+
+
+    A sample response for the above request is shown below:
+
+    
+
+            {
+                "rates": [
+                    {
+                        "service_name": "Standard Delivery",
+                        "service_code": "standard",
+                        "total_price": "9422.0",
+                        "description": "Pickup and delivery within 3 - 5 business days",
+                        "currency": "USD",
+                        "max_delivery_date": "2020-09-18"
+                    }
+                ]
+            }
     """
 
     post_data = request.data['rate']
@@ -46,12 +134,14 @@ def shipping_rates(request, *args, **kwargs):
     
     # Parse required variables from incoming POST request
     origin_country = post_data['origin']['country']
+
     # Convert Country Code to full for SendBox API
     if len(origin_country) == 2:
         origin_country = country_info[origin_country]    
     origin_state = post_data['origin']['province']     
     origin_city = post_data['origin']['city']     
     destination_country = post_data['destination']['country']
+
     # Convert Country Code to full for SendBox API
     if len(destination_country) == 2:
         destination_country = country_info[destination_country]      
--- shopify_sendbox/shopify_sendbox/settings.py
+++ shopify_sendbox/shopify_sendbox/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SETTINGS_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shopify-sendbox-prod.herokuapp.com', 'shopify-sendbox-staging.herokuapp.com', '127.0.0.1']
 
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# ]
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 
